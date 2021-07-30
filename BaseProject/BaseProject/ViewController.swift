8dc523da2b13170995a6f854000b37568dcf10a3
김태현 <taehyk528@gmail.com>
2021-07-30 23:58:44 +0900

    [CHORE] Create Project (#5)

---
+++ BaseProject/BaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by taehy.k on 2021/07/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
